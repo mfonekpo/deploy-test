683b7d1d829dbcc784d423b1bfe9392c4329e7c0
mfonekpo <mfonekpo34@gmail.com>
2024-12-06 18:33:55 +0100

    second commit

--- prefect_gitStorage.py
+++ prefect_gitStorage.py
@@ -1,6 +1,10 @@
 from prefect import flow
 
 
+@flow(log_prints=True)
+def my_flow(name: str = "world"):
+    print(f"Hello, {name}!")
+
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/mfonekpo/deploy-test.git",
